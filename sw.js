2fb68d2eb332c409072e733e16cc8f9788724062
shong84 <shong84@gmail.com>
2026-08-12 10:42:07 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='988-fm-official-logo-v1';
-const ASSETS=['./','./index.html','./manifest.json','./988-logo-original.png','./favicon.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if (new URL(e.request.url).origin === location.origin) {
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-  }
-});
