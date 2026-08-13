de0586fffd7bb85d951bc46bf58ae1f198924411
shong84 <shong84@gmail.com>
2026-08-13 16:24:50 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='988-fm-recovered-autoplay-v5';
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).then(r => {
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match(e.request)));
-});
